625f6d6b7936d2520720a0faf8def4655caf5880
Martin Schütte <m.schuette-extern@xplosion.de>
2015-07-22 16:10:38 +0200

    fix cache dir ownership

--- recipes/_user.rb
+++ recipes/_user.rb
@@ -33,3 +33,9 @@ directory node['jenkins']['master']['cache_directory'] do
   mode      '0755'
   recursive true
 end
+
+# fix cache dir ownership (necessary if it already existed)
+execute "chown_#{node['jenkins']['master']['cache_directory']}" do
+  command "chown -R #{node['jenkins']['master']['user']}:#{node['jenkins']['master']['group']} #{node['jenkins']['master']['cache_directory']}"
+  not_if { Etc.getpwuid(File.stat("#{node['jenkins']['master']['cache_directory']}/war").uid).name != "node['jenkins']['master']['user']" }
+end
